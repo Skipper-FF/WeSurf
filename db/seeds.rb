587b75a66c2be51758d07eb36ee274683bf81ddf
Francois Fauvel <francoisfauvel@outlook.fr>
2021-02-15 17:29:45 +0100

    starts seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Board.destroy_all
+Booking.destroy_all
+
+@user = User.create(email:"test@gmail.com", password: "123456")
+Board.create(user_id:@user.id, description:"The Duende model is an interpretation of a classic pig model originally created in the early 60’s. It is a playful board with a lot of volume around the stringer with a D fin, and foiled from the nose to the tail. It is only shaped in high density foam, allowing for weight and momentum.")
