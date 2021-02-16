f873f72f64d3e688df4d72e5a004f26e263d0a28
marc mizrahi <mizrahimarc@gmail.com>
2021-02-16 10:58:41 +0100

    pushed the rooute update

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   resources :users, only: :show
   resources :boards
-  resources :photos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    resources :boards do
     resources :booking, only: [:new]
