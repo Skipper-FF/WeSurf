ac37865a6a915bbb0673a08567b7c743dc8d5685
Francois Fauvel <francoisfauvel@outlook.fr>
2021-02-16 11:26:30 +0100

    columns added to users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
              path: '',
              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile'},
              controllers: {
-                 omniauth_callbacks: 'omniauth_callbacks',
                  registrations: 'registrations'
              }
 
