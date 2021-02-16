66e68135a59374561ed6004d02d3ca96fb8afe66
agathesf <76791952+agathesf@users.noreply.github.com>
2021-02-16 11:12:24 +0100

    Update user.rb
    je viens de corriger une faute de syntaxe

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :bookings
 
   validates :email, presence: true, uniqueness: true
-  validates :firs_name, presence: true
+  validates :first_name, presence: true
   validates :last_name, presence: true
   validates :encrypted_password, presence: true
 end
