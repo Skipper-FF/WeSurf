a78ed21862188bcc61619076b34445898eb1ce03
Agathe Saint-Frison <agathe.saintfrison@orange.fr>
2021-02-16 10:44:30 +0100

    Correct index confrontation

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,23 @@
 class BoardsController < ApplicationController
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to board_path(@board)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+  
+  def index
+    @boards = Board.all
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
+  root 'pages#home'
+
+  devise_for :users,
+             path: '',
+             path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile'},
+             controllers: {
+                 omniauth_callbacks: 'omniauth_callbacks',
+                 registrations: 'registrations'
+             }
+
+  resources :users, only: :show
+  resources :boards
+  resources :photos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    resources :boards do
     resources :booking, only: [:new]
