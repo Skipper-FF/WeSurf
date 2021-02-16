1d03b3e91396b693ba26903c77bb6f12e4989091
Agathe Saint-Frison <agathe.saintfrison@orange.fr>
2021-02-16 12:55:25 +0100

    All controller updated

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,27 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
 
-  def preload
-    board = Board.find(params[:board_id])
-    today = Date.today
-    bookings = board.bookings.where("start_date >= ? OR end_date >= ?", today, today)
+  # def preload
+  #   board = Board.find(params[:board_id])
+  #   today = Date.today
+  #   bookings = board.bookings.where("start_date >= ? OR end_date >= ?", today, today)
 
-    render json: bookings
+  #   render json: bookings
+  # end
+
+  def new
+    @board = Board.find(params[:board_id])
+    @booking = Booking.new
   end
 
   def create
     @booking = current_user.bookings.create(booking_params)
-
     redirect_to @booking.board, notice: "Your booking has been created..."
   end
 
   private
 
   def booking_params
-    params.require(booking).permit(:start_date, :end_date, :price, :total, :board_id)
+    params.require(booking).permit(:start_date, :end_date, :price, :board_id, :user_id)
   end
-  
 end
