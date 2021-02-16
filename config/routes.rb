8235cb975976edbbf74bb55dbc96f946994e3a64
marc mizrahi <mizrahimarc@gmail.com>
2021-02-16 12:01:10 +0100

    updated route

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,17 @@ Rails.application.routes.draw do
 
   devise_for :users,
              path: '',
-             path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile'},
-             controllers: {
-                 registrations: 'registrations'
-             }
+             path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile'}
+            #  controllers: {
+            #      bookings: 'bookings'
+            #  }
 
   resources :users, only: :show
-  resources :boards
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   resources :boards do
-    resources :booking, only: [:new]
+  resources :boards do
+    resources :booking, only: [:new, :create]
   end
+
+  resources :bookings, only [:index, :show]
+  get 'dashboard', to 'dashboard#dashboard'
 end
