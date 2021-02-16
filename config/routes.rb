daaacc32cc9a46523da9209abf821e04553037de
marc mizrahi <mizrahimarc@gmail.com>
2021-02-16 12:19:48 +0100

    pushed

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   end
 
   resources :bookings, only: [:index, :show]
-  get 'dashboard', to 'dashboard#dashboard'
+  # get 'dashboard', to 'dashboard#dashboard'
 end
