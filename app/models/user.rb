ed8e864ec9f1051818dbe3fae63e0a31dc5f7945
marc mizrahi <mizrahimarc@gmail.com>
2021-02-18 16:45:07 +0100

    worked on the map box and fixed some issue all over the place

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :boards
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
